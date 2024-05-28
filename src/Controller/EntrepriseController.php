59a774e421eac9f4bf043b93071d2fb408ddab98
Ali Marzak <ali.marzak@live.fr>
2024-05-28 10:32:14 +0200

    Ajout Projet Symfony : Tuto Entreprise

---
+++ src/Controller/EntrepriseController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
+use App\Repository\EntrepriseRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EntrepriseController extends AbstractController
+{
+    #[Route('/entreprise', name: 'app_entreprise')]
+    public function index(EntrepriseRepository $entrepriseRepository): Response
+    {
+        // $entreprises = $entrepriseRepository->findAll();
+        $entreprises = $entrepriseRepository->findBy([], ["raisonSociale" => "ASC"]);
+        return $this->render('entreprise/index.html.twig', [
+            'entreprises' => $entreprises
+        ]);
+    }
+
+    #[Route('/entreprise/new', name: 'new_entreprise')]
+    #[Route('/entreprise/{id}/edit', name: 'edit_entreprise')]
+    public function new_edit(Entreprise $entreprise = null, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        if(!$entreprise) {
+            $entreprise = new Entreprise();
+        }
+
+        $form = $this->createForm(EntrepriseType::class, $entreprise);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $entreprise = $form->getData();
+
+            $entityManager->persist($entreprise);
+            $entityManager->flush();
+            
+            return $this->redirectToRoute('app_entreprise');
+        }
+
+        return $this->render('entreprise/new.html.twig', [
+            'formAddEntreprise' => $form,
+            'edit' => $entreprise->getId()
+        ]);
+    }
+
+    #[Route('/entreprise/{id}/delete', name: 'delete_entreprise')]
+    public function delete(Entreprise $entreprise, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->remove($entreprise);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_entreprise');
+    }
+
+    // ordre de priorité des routes
+    #[Route('/entreprise/{id}', name: 'show_entreprise')]
+    public function show(Entreprise $entreprise): Response
+    {
+        return $this->render('entreprise/show.html.twig', [
+            'entreprise' => $entreprise
+        ]);
+    }
+}
